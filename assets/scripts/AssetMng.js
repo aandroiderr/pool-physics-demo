676a5796a19f1d465d26e67d7627389567c17d76
Nan Wang <nantas@gmail.com>
2015-12-06 02:52:54 +0800

    prepared art assets.

---
+++ assets/scripts/AssetMng.js
@@ -0,0 +1,21 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        ballImages: {
+            default: [],
+            type: [cc.SpriteFrame],
+            visible: true
+        }
+    },
+
+    // use this for initialization
+    onLoad: function () {
+
+    },
+
+    // called every frame
+    update: function (dt) {
+
+    },
+});
---
+++ assets/scripts/Ball.js
@@ -0,0 +1,16 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        number: 0,
+    },
+
+    // use this for initialization
+    onLoad: function () {
+    },
+
+    // called every frame
+    update: function (dt) {
+
+    },
+});
